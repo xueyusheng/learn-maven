168d15c94c4531c71d73c35f0bb99618f6a6b233
xueyusheng <xueyusheng@zxisl.com>
2016-08-11 14:55:28 +0800

    d

---
+++ src/main/java/xueys/test/learn_maven/App.java
@@ -0,0 +1,16 @@
+package xueys.test.learn_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void test(){
+		System.out.println("sdfsdfdsfsdfsdf");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
